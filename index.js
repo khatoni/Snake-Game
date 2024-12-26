3cd0e2baffc9443973f9246b73e51e98e78eba46
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-12-26 22:47:49 +0200

    init express server

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-console.log("Hello");
+const express = require('express');
 
-console.log("World");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+  res.end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}. Visit http://localhost:${PORT}`);
+});
