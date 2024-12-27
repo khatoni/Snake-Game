363256fb9e5d7f9beda38631f33db07f6081112a
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-12-27 02:09:08 +0200

    init ejs

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/pages'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-  res.end();
+  res.render('home');
 });
 
 app.listen(PORT, () => {
