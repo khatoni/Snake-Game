63a13446f3fa4363db2634a6e45cc41a13acfb82
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-12-27 02:29:09 +0200

    add sockets

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const WebSocket = require('ws');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const webSocketServer = new WebSocket.Server({ server });
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/pages'));
@@ -12,6 +16,26 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.listen(PORT, () => {
+webSocketServer.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.send('Welcome to the WebSocket server!');
+  ws.on('message', (message) => {
+      console.log('Received:', message.toString());
+      
+      // Broadcast to all clients
+      webSocketServer.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+              client.send(`Server broadcast: ${message}`);
+          }
+      });
+  });
+
+  ws.on('close', () => {
+      console.log('Client disconnected');
+  });
+});
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}. Visit http://localhost:${PORT}`);
 });
