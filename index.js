279d98cf6ffcbcb9027d7d6a86b9eaa3804ce600
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-02-09 00:34:01 +0200

    some styling

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
 app.get('/pages/single-player', (req, res) => {
   res.render('single-player');
 });
