2c14c33c278ce68a0a1bd452b31ef82eeca9c8fe
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-02-11 23:56:04 +0200

    fixes

--- infrastructure/ws-server.js
+++ infrastructure/ws-server.js
@@ -1,183 +1,181 @@
-const WebSocket = require('ws');
-const PriorityQueue = require('./priority_queue');
+const WebSocket = require("ws");
+const PriorityQueue = require("./priority_queue");
 
 const roomsIds = new PriorityQueue();
-for(let i = 0; i < 20; i++) {
-    roomsIds.push(i);
+for (let i = 0; i < 20; i++) {
+	roomsIds.push(i);
 }
 
 const searchRandom = new Set();
 
 function configureWsServer(server) {
-    const webSocketServer = new WebSocket.Server({ server });
+	const webSocketServer = new WebSocket.Server({ server });
 
-    const generatedGUIDS = new Set();
-    // guid => socket
-    const guidToSocket = new Map();
-    const socketToGuid = new Map(); // ?
-    // rooms object = {};
-    // key -> room id, value -> array of guids/sockets
-    const rooms = [];
-    // guid both 
-    // first sends event => vtoriq guid
-    // lobby s id 1: {guid1: 1, guid2: 2}
+	const generatedGUIDS = new Set();
+	const guidToSocket = new Map();
+	// rooms object = {};
+	// key -> room id, value -> array of guids/sockets
+	const rooms = [];
+	const guidToRoom = new Map();
 
-    // key - user guid
-    // value - room id
-    const guidToRoom = new Map();
+	webSocketServer.on("connection", (ws) => {
+		// create guid and send to client
+		const myGuid = createGUID(generatedGUIDS);
+		guidToSocket.set(myGuid, ws);
 
-    webSocketServer.on('connection', (ws) => {
-        // create guid and send to client
-        const myGuid = createGUID(generatedGUIDS);
-        guidToSocket.set(myGuid, ws);
+		const connectionEvent = {
+			name: "connection",
+			guid: myGuid,
+			message: myGuid,
+		};
 
-        const connectionEvent = {
-            name: 'connection',
-            guid: myGuid,
-            message: myGuid
-        };
+		ws.send(JSON.stringify(connectionEvent));
+		ws.on("message", (message) => {
+			const event = JSON.parse(message);
 
-        ws.send(JSON.stringify(connectionEvent));
-        ws.on('message', (message) => {
-            const event = JSON.parse(message);
+			// TODO: create object of handlers
+			if (event.name === "joinMeWith") {
+				const otherGuid = event.data;
+				if (!checkExistingGuid(guidToSocket, otherGuid)) {
+					ws.send(`There is not such guid existing: ${otherGuid}`);
+					return;
+				}
+				if (myGuid === otherGuid) {
+					ws.send(`You cannot enter the same guid`);
+					return;
+				}
 
-            // TODO: create object of handlers
-            if (event.name === 'joinMeWith') {
-                const otherGuid = event.data;
-                if(!checkExistingGuid(guidToSocket, otherGuid)) {
-                    ws.send(`There is not such guid existing: ${otherGuid}`);
-                    return;
-                }
-                if(myGuid === otherGuid) {
-                    ws.send(`You cannot enter the same guid`);
-                    return;
-                }
+				if (searchRandom.has(otherGuid) || searchRandom.has(myGuid)) {
+					// TODO: decide what to do
+					return;
+				}
 
-                const myRoom = joinRoom(rooms, guidToSocket, myGuid, otherGuid);
-                guidToRoom.set(myGuid, myRoom);
-                guidToRoom.set(otherGuid, myRoom);
-            }
-            else if(event.name === 'moveSnake') {
-                const myRoom = guidToRoom.get(myGuid);
-                let player = event.player;
-                let players = rooms[myRoom].guids;
-                let direction = event.direction;
-                const moveEvent = {
-                    name: 'moveSnake',
-                    player: player,
-                    direction: direction
-                };
-                players.forEach((player) => {
-                    if(guidToSocket.get(player).readyState === WebSocket.OPEN) {
-                        guidToSocket.get(player).send(JSON.stringify(moveEvent));
-                    }
-                });
-            }
-            else if(event.name === 'generateFood') {
-                let gameState = event.gameState;
-                generateFood(rooms, guidToRoom.get(myGuid), gameState, guidToSocket);
-            }
-            else if(event.name === 'searchRandom') {
-                searchRandom.add(myGuid);
-                if(searchRandom.size >= 2) {
-                    searchRandom.delete(myGuid);
-                    let otherGuid = "";
-                    // get some element from set
-                    for(let guid of searchRandom) {
-                        if(guid) {
-                            otherGuid = guid;
-                            break;
-                        }
-                    }
-                    searchRandom.delete(otherGuid);
-                    
-                    const myRoom = joinRoom(rooms, guidToSocket, myGuid, otherGuid);
-                    guidToRoom.set(myGuid, myRoom);
-                    guidToRoom.set(otherGuid, myRoom);
-                    searchRandom.clear();
-                }
-            }
-        });
-      
-        ws.on('close', () => {
-            console.log('Client disconnected');
-        });
-    });
+				joinRoom(rooms, guidToSocket, myGuid, otherGuid);
+			} else if (event.name === "moveSnake") {
+				const myRoom = guidToRoom.get(myGuid);
+				let player = event.player;
+				let players = rooms[myRoom].guids;
+				let direction = event.direction;
+				const moveEvent = {
+					name: "moveSnake",
+					player: player,
+					direction: direction,
+				};
+				players.forEach((player) => {
+					if (
+						guidToSocket.get(player).readyState === WebSocket.OPEN
+					) {
+						guidToSocket
+							.get(player)
+							.send(JSON.stringify(moveEvent));
+					}
+				});
+			} else if (event.name === "generateFood") {
+				let gameState = event.gameState;
+				generateFood(
+					rooms,
+					guidToRoom.get(myGuid),
+					gameState,
+					guidToSocket
+				);
+			} else if (event.name === "searchRandom") {
+				searchRandom.add(myGuid);
+				if (searchRandom.size >= 2) {
+					searchRandom.delete(myGuid);
+					let otherGuid = "";
+					// get some element from set
+					for (let guid of searchRandom) {
+						if (guid) {
+							otherGuid = guid;
+							break;
+						}
+					}
+					searchRandom.delete(otherGuid);
 
-    return webSocketServer;
+					joinRoom(rooms, guidToRoom, guidToSocket, myGuid, otherGuid);
+				}
+			}
+		});
+
+		ws.on("close", () => {
+			searchRandom.delete(myGuid);
+		});
+	});
+
+	return webSocketServer;
 }
-
-module.exports = configureWsServer;
 
 function createGUID(generatedGUIDS) {
-    const possibleSymbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    while (true) {
-        let guid="";
-        for (let i = 0; i < 5; i++) {
-            guid += possibleSymbols.charAt(Math.floor(Math.random() * possibleSymbols.length));
-        }
-        if (!generatedGUIDS.has(guid)) {
-            generatedGUIDS.add(guid);
-            return guid;
-        }
-    }
+	const possibleSymbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	while (true) {
+		let guid = "";
+		for (let i = 0; i < 5; i++) {
+			guid += possibleSymbols.charAt(
+				Math.floor(Math.random() * possibleSymbols.length)
+			);
+		}
+		if (!generatedGUIDS.has(guid)) {
+			generatedGUIDS.add(guid);
+			return guid;
+		}
+	}
 }
 
-function joinRoom(rooms, guidToSocket, myGuid, otherGuid) {
-    if(searchRandom.has(myGuid) || searchRandom.has(otherGuid)) {
-        // TODO: something went completely wrong
-        return;
-    }
-    let freeRoomId = roomsIds.pop();
-    let room = {id:freeRoomId, guids:[myGuid, otherGuid]};
-    rooms.push(room);
- 
-    const event = {
-        name: 'startGame',
-        data: {
-            roomId: freeRoomId,
-            startTime: new Date().getTime() + 5,
-            players: [myGuid, otherGuid]
+function joinRoom(rooms, guidToRoom, guidToSocket, myGuid, otherGuid) {
+	let freeRoomId = roomsIds.pop();
+	let room = { id: freeRoomId, guids: [myGuid, otherGuid] };
+	rooms.push(room);
 
-        }
-    };
-    event.data[myGuid] = {
-        position: {x: 2, y: 2},
-        direction: {x: 1, y: 0}
-    }
-    event.data[otherGuid] = {
-        position: {x: 18, y: 18},
-        direction: {x: -1, y: 0}
-    };
-    
-    guidToSocket.get(otherGuid).send(JSON.stringify(event));
-    guidToSocket.get(myGuid).send(JSON.stringify(event));
-    return rooms.length - 1;
+	const event = {
+		name: "startGame",
+		data: {
+			roomId: freeRoomId,
+			startTime: new Date().getTime() + 5,
+			players: [myGuid, otherGuid],
+		},
+	};
+	event.data[myGuid] = {
+		position: { x: 2, y: 2 },
+		direction: { x: 1, y: 0 },
+	};
+	event.data[otherGuid] = {
+		position: { x: 18, y: 18 },
+		direction: { x: -1, y: 0 },
+	};
+
+	guidToSocket.get(otherGuid).send(JSON.stringify(event));
+	guidToSocket.get(myGuid).send(JSON.stringify(event));
+
+	const myRoom = rooms.length - 1;
+	guidToRoom.set(myGuid, myRoom);
+	guidToRoom.set(otherGuid, myRoom);
 }
 
 function checkExistingGuid(guidToSocket, guid) {
-    return guidToSocket.has(guid);
+	return guidToSocket.has(guid);
 }
 
 function generateFoodCoordinates(gameState) {
-	while(true) {
+	while (true) {
 		let foodX = Math.round(Math.random() * 20);
 		let foodY = Math.round(Math.random() * 20);
-		if(!gameState[foodX][foodY]) {
-			return {x: foodX, y: foodY};
+		if (!gameState[foodX][foodY]) {
+			return { x: foodX, y: foodY };
 		}
 	}
 }
 
 function generateFood(rooms, roomIndex, gameState, guidToSocket) {
-    let foodCoordinates = generateFoodCoordinates(gameState);
-    const event = {
-        name: 'generateFood',
-        food: foodCoordinates
-    };
-    rooms[roomIndex].guids.forEach((guid) =>{
-        if(guidToSocket.get(guid).readyState === WebSocket.OPEN) {
-            guidToSocket.get(guid).send(JSON.stringify(event))
-        }
-    });
+	let foodCoordinates = generateFoodCoordinates(gameState);
+	const event = {
+		name: "generateFood",
+		food: foodCoordinates,
+	};
+	rooms[roomIndex].guids.forEach((guid) => {
+		if (guidToSocket.get(guid).readyState === WebSocket.OPEN) {
+			guidToSocket.get(guid).send(JSON.stringify(event));
+		}
+	});
 }
+
+module.exports = configureWsServer;
